76804ce5a816faf58adfb8a74054f7bfc53d0d36
marco bottoni <bottoni.mrc@gmail.com>
2020-11-19 17:06:19 +0100

    controllo numero utente

---
+++ js/campominato.js
@@ -0,0 +1,33 @@
+// Descrizione
+// Il computer deve generare 16 numeri casuali tra 1 e 100.
+// I numeri non possono essere duplicati.
+// In seguito deve chiedere all’utente (100 - 16) volte di inserire un numero alla volta, sempre compreso tra 1 e 100.
+// L’utente non può inserire più volte lo stesso numero.
+// Se il numero è presente nella lista dei numeri generati, la partita termina, altrimenti si continua chiedendo all’utente un altro numero.
+// La partita termina quando il giocatore inserisce un numero “vietato” o raggiunge il numero massimo possibile di numeri consentiti.
+// Al termine della partita il software deve comunicare il punteggio, cioè il numero di volte che l’utente ha inserito un numero consentito.
+// BONUS: (da fare solo se funziona tutto il resto)
+// all’inizio il software richiede anche una difficoltà all’utente che cambia il range di numeri casuali:
+// con difficoltà 0 => tra 1 e 100
+// con difficoltà 1 =>  tra 1 e 80
+
+var arrayBombe =[];
+
+
+while (arrayBombe.length < 16) {
+  var bombe = Math.floor(Math.random()*100)+1;
+  if (!arrayBombe.includes(bombe)) {
+    arrayBombe.push(bombe);
+  }
+}
+console.log(arrayBombe);
+
+var arrayNumeroUtente = [];
+
+while (arrayNumeroUtente.length < 4) {
+  var numeroUtente = parseInt(prompt("inserisci numero"));
+  if ((!arrayBombe.includes(numeroUtente)) && (!arrayNumeroUtente.includes(numeroUtente)) ) {
+    arrayNumeroUtente.push(numeroUtente)
+  }
+  console.log(numeroUtente);
+}
